d341167761c04abf27042dcf3ad1b61557ad53c9
Devon Wieczorek <devon.wieczorek93@gmail.com>
2019-08-14 12:19:14 -0400

    Pull options from .env variables
    Replace site_abbv when saving settings

--- wordpress-site/sync-db.sh
+++ wordpress-site/sync-db.sh
@@ -1,32 +1,41 @@
 # Pull in variables from .env
 source .env
 
-# prompt for which container to sync db to
-echo "enter in the domain of the container you want to sync the db to."
-echo "ex: $local_docker_domain"
+# Check if local url is set as an env var
+if [[ ! -z $local_docker_domain ]]
+then
+    container_domain=$local_docker_domain
+else
+    echo ""
+    echo "Enter in the domain of the LOCAL url you want to use in the database"
+    echo "ex: local.finddreamjobs.com"
+    read container_domain
+fi
 
-read container_domain
-
-echo "enter in the STAGING WPENGINE site name"
+# Get the staging environment to copy the SQL file from
+echo "Enter in the STAGING WPEngine site name"
 echo "ex: fdjstaging"
-
 read wpengine_sitename
 
-# copy exported sql file to local machine
-scp ${wpengine_sitename}@${wpengine_sitename}.ssh.wpengine.net:/sites/${wpengine_sitename}/db-backup-sync.sql db-backup-sync.sql
+# Copy exported sql file to local machine
+scp $wpengine_sitename@$wpengine_sitename.ssh.wpengine.net:/sites/$wpengine_sitename/db-backup-sync.sql db-backup-sync.sql
+echo "Copied SQL file successfully."
 
-echo "copied sql file successfully."
-
-# import sql file to local db
+# Import sql file to local db
 cat db-backup-sync.sql | docker exec -i local_${site_abbreviation}_db /usr/bin/mysql -u root --password=root wordpress
+echo "Imported SQL file to database successfully."
 
-echo "imported sql file to database successfully."
-
-echo "enter in the PRODUCTION WPENGINE domain for this db WITHOUT the trailing slash"
-echo "this is needed to do a search and replace in the db with your local domain"
-echo "ex: $production_url"
-
-read wpengine_domain
+# Check if production url is set as an env var
+if [[ ! -z $production_url ]]
+    then
+    wpengine_domain=$production_url
+else
+    echo ""
+    echo "Enter in the PRODUCTION WPENGINE domain for this db WITHOUT the trailing slash"
+    echo "This is needed to do a search and replace in the db with your local domain"
+    echo "ex: https://www.finddreamjobs.com"
+    read wpengine_domain
+fi
 
 # start bash session on local db container so we can search and replace inside new sql file
 docker exec -it local_${site_abbreviation}_db sh -c "mysql -u root -proot wordpress -e 'UPDATE wp_options SET option_value = replace(option_value, \"$wpengine_domain\", \"https://$container_domain\") WHERE option_name = \"home\" OR option_name = \"siteurl\"';"
@@ -41,20 +50,21 @@ docker exec -it local_${site_abbreviation}_db sh -c "mysql -u root -proot wordpr
 
 echo "replaced all old url references successfully."
 
-echo "do you want to save this configuration?"
+# Prompt user to save configuration
+echo "Do you want to save this configuration?"
 echo "yes/no?"
-
 read save_configuration
 
-# if user wants to save config, copy sample job file and replace vars
+# If user wants to save config, copy sample job file and replace vars
 if [ $save_configuration = "yes" ]; then
 
-    # copy file
+    # Copy file
     cp sample-sync-db-config.sh $container_domain-sync-db.sh
 
-    # replace variables
+    # Replace variables
     sudo sed -i '' 's/$container_domain/'$container_domain'/g' $container_domain-sync-db.sh
     sudo sed -i '' 's/$wpengine_sitename/'$wpengine_sitename'/g' $container_domain-sync-db.sh
+    sudo sed -i '' 's/${site_abbreviation}/'$site_abbreviation'/g' $container_domain-sync-db.sh
     sudo sed -i '' 's,$wpengine_domain,'$wpengine_domain',g' $container_domain-sync-db.sh
 
     echo "you can now run this configuration from $container_domain-sync-db.sh"
